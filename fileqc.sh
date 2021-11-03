18ffa598cd38973e32a136fb437da15f19c28546
union-leachr <leachr@union.edu>
2021-11-03 11:50:59 -0400

    Add dimensions

--- fileqc.sh
+++ fileqc.sh
@@ -6,17 +6,18 @@ c=1
 IPATH=$1
 PATHNAME="$(basename -- $IPATH)"
 NUMFILES="$(ls $IPATH | wc -l | xargs echo -n)"
-echo "filename", "mimetype", "size", "bits", "dpi", "color" > "$PATHNAME.csv"
+echo "filename", "mimetype", "size", "dimensions", "bits", "color", "dpi" > "$PATHNAME.csv"
 for i in $(find $IPATH \( -name "*.tiff" -o -name "*.tif" -o -name "*.jpg" \));
 do
-	f="$(basename -- $i)"
 	echo "Processing $c of $NUMFILES files"
+	f="$(basename -- $i)"
 	mime="$(exiftool -p '$MimeType' $i)" 
 	size="$(exiftool -p '$FileSize' $i)"
+	dimensions="$(exiftool -p '$ImageSize' $i)"
 	bits="$(exiftool -p '$BitsPerSample' $i)"
 	dpi="$(exiftool -p '$XResolution;$YResolution' $i)"
 	color="$(exiftool -p '$ColorSpaceData' $i)"
-	echo "$f," "$mime", "$size", "$bits", "$dpi", "$color" >> "$PATHNAME.csv"
+	echo "$f," "$mime", "$size", "$dimensions", "$bits", "$color", "$dpi"  >> "$PATHNAME.csv"
 	let c=c+1
 done;
 
